9b2f009e2c9bd44342907623c028379b0df0cc17
Bryan Burns <bryanburns@gmail.com>
2026-08-05 13:38:05 -0700

    Add effectiveThreshold as the single source of truth for player minimums

--- src/lib/schedule/index.ts
+++ src/lib/schedule/index.ts
@@ -1,6 +1,7 @@
 export * from './suggestions';
 export * from './scheduling';
 export * from './scheduleView';
+export * from './threshold';
 export * from './upcomingSessions';
 export * from './otherGameSessions';
 export * from './gameHealth';
